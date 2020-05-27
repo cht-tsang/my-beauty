65bfba5510519f2a4d430cc53d837faf29460165
Anastasiia Makarova <erdbeer@yandex.ru>
2020-05-27 14:16:57 +0100

    Add seeds for treatments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Treatment.destroy_all
+Beautician.destroy_all
+User.destroy_all
+
+user = User.create(email: "anna@gmail.com", password: "1234567")
+beautician = Beautician.create(description: "Haircuts", location: "London", name: "Anna Haircuts", user: user)
+Treatment.create(name: "Ladies' Haircuts", cost: 30, description: "Trendy Haircuts for Women", category: "Hair", beautician: beautician)
+Treatment.create(name: "Facial Threading", cost: 10, description: "Threading allows for a more defined and precise shape and can create better definition for eyebrows", category: "Hair removal", beautician: beautician)
+Treatment.create(name: "Face Massage", cost: 35, description: "Face massage may be used to relieve tension and possibly control age-related phenomena, such as wrinkling.", category: "Massage", beautician: beautician)
+Treatment.create(name: "Manicure", cost: 20, description: "A manicure is a beauty treatment which uses special tools, creams, waxes and / or massage techniques to leave your nails and hands healthy", category: "Nails", beautician: beautician)
+Treatment.create(name: "Eyelash Extensions", cost: 40, description: "Eyelash Extensions is a method used by eyelash technicians to enhance the length, thickness, and fullness of natural eyelashes", category: "Face", beautician: beautician)
+Treatment.create(name: "Ladies' Haircuts", cost: 30, description: "Trendy Haircuts for Women", category: "Hair", beautician: beautician)
