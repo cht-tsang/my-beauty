c6df7e0936201c2e2a5942be087a16d0971c2e06
chi tsang <cht.chihungtsang@gmail.com>
2020-05-27 15:58:41 +0100

    fixed booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :treatment
 
-  validates :date, presence: true
-  validates :time, presence: true
+  validates :date, presence: true, on: :update
+  validates :time, presence: true, on: :update
   # validates :status, presence: true
 end
