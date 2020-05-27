6a29f7c9b6dd0af8f3fdd37b822c6dd4afc3f958
chi tsang <cht.chihungtsang@gmail.com>
2020-05-27 12:43:27 +0100

    added location to bookings table

---
+++ db/migrate/20200527114041_add_location_to_bookings.rb
@@ -0,0 +1,5 @@
+class AddLocationToBookings < ActiveRecord::Migration[6.0]
+  def change
+    add_column :bookings, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_102247) do
+ActiveRecord::Schema.define(version: 2020_05_27_114041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_102247) do
     t.bigint "treatment_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
     t.index ["treatment_id"], name: "index_bookings_on_treatment_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
