e79f2bd2cf17e2d446aa5fb6756000168b9e9d09
chi tsang <cht.chihungtsang@gmail.com>
2020-05-28 12:39:25 +0100

    payment changes saved

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
+
   def create
     booking = Booking.find(params[:booking_id])
     order  = Order.create!(booking: booking, booking_sku: booking.sku, amount: booking.price, state: 'pending', user: current_user)
