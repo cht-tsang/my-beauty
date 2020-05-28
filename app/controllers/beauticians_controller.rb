325fb867b23c62a49abd9e53d6a2869673fd7d11
Anastasiia Makarova <erdbeer@yandex.ru>
2020-05-28 16:46:01 +0100

    Add datepicker to the field and linked form to the index page

--- app/controllers/beauticians_controller.rb
+++ app/controllers/beauticians_controller.rb
@@ -2,12 +2,24 @@ class BeauticiansController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    
-    if params[:query].present?
-      @beauticians = Beautician.global_search(params[:query])
-      respond_to do |format|
-        format.js
+
+    if params[:query].present? || params[:multiquery].present?
+      if params[:query].present?
+        @beauticians = Beautician.global_search(params[:query])
+      elsif params[:multiquery].present?
+        @beauticians = Beautician.all
+        if params[:multiquery][:treatment]
+          @beauticians = @beauticians.select{|beautician| beautician.treatments.include?(Treatment.find(params[:multiquery][:treatment].to_i))}
+        end
+        if params[:multiquery][:postcode]
+        end
+        if params[:multiquery][:date]
+        end
+          # @beauticians = @beauticians
       end
+      # respond_to do |format|
+      #   format.js
+      # end
     else
       @beauticians = Beautician.all
     end
@@ -17,7 +29,7 @@ class BeauticiansController < ApplicationController
     @beautician = Beautician.new
   end
 
-  def create 
+  def create
     @beautician = Beautician.new(beautician_params)
     @beautician.user = current_user
     if @beautician.save
@@ -32,7 +44,7 @@ class BeauticiansController < ApplicationController
   end
 
   private
-  
+
   def beautician_params
     params.require(:beautician).permit(:name, :location, :description, :banner_photo, portfolio_photos: [], treatment_ids: [] )
   end
