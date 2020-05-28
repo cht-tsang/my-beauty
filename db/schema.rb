89ede327706b7136c27bd2325b454cbe55e0f526
chi tsang <cht.chihungtsang@gmail.com>
2020-05-28 11:55:20 +0100

    added sku column to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_200554) do
+ActiveRecord::Schema.define(version: 2020_05_28_105402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_200554) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "location"
     t.integer "price_cents", default: 0, null: false
+    t.string "sku"
     t.index ["treatment_id"], name: "index_bookings_on_treatment_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
