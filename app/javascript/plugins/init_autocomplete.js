3ad7fb2ec25176517d6ea5281be6da7e80e89d7a
chi tsang <cht.chihungtsang@gmail.com>
2020-06-01 10:56:45 +0100

    added javascript for autocomplete front page postcode

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,3 +1,10 @@
 import places from 'places.js';
 
+const initAutocomplete = () => {
+  const addressInput = document.getElementById('your-postcode');
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+};
+
 export { initAutocomplete };
