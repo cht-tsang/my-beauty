01ca9951bf0a84cc0b6e63410c59a8b19428d043
Marco Martins <marco_martins909@hotmail.co.uk>
2020-06-01 16:47:04 +0100

    sorted out the migration files for bookings

--- db/migrate/20200601090122_add_beautician_to_booking.rb
+++
@@ -1,5 +0,0 @@
-class AddBeauticianToBooking < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :bookings, :beautician, null: false, foreign_key: true
-  end
-end
--- db/migrate/20200601140916_remove_beautician_reference_from_booking_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBeauticianReferenceFromBookingTable < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :bookings, :beautician, foreign_key: true
-  end
-end
